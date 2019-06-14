18054b9de3e2090467e89be37cfcd32bd2004fdb
mick3247652 <mick.dunaev@yandex.ru>
2019-06-14 23:35:48 +0300

    keyboard dismiss off

--- App/components/Container/Container.js
+++ App/components/Container/Container.js
@@ -3,23 +3,18 @@ import PropTypes from "prop-types";
 import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
 import styles from "./styles";
 
-const Container = ({ children, backgroundColor}) => {
-  
-    const containerStyle = [styles.container]
-    if(backgroundColor) {
-      containerStyle.push({backgroundColor})
-    }
-    
-    return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={containerStyle}>{children}</View>
-    </TouchableWithoutFeedback>
-  );
-    }
-  
-  Container.propTypes = {
-    children: PropTypes.any,
-    backgroundColor: PropTypes.string,
-  };
-  
-  export default Container;
+const Container = ({ children, backgroundColor }) => {
+  const containerStyle = [styles.container];
+  if (backgroundColor) {
+    containerStyle.push({ backgroundColor });
+  }
+
+  return <View style={containerStyle}>{children}</View>;
+};
+
+Container.propTypes = {
+  children: PropTypes.any,
+  backgroundColor: PropTypes.string
+};
+
+export default Container;
